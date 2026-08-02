67e8ffd00b83c462a7cf6c44e023a69c9bed0eaa
sarkarmamun171 <sarkar.pau.cse171@gmail.com>
2026-08-02 23:43:35 +0600

    Added by routes api php

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,8 +4,27 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request) {
+        $request->validate([
+             'name' => 'required|string|max:255',
+             'email' => 'required|email|unique:users,email',
+             'password' => 'required|min:8',
+        ]);
+
+            $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+                'message' => 'Registration successful',
+                'user' => $user,
+        ],201);
+    }
 }
