747e256129a7b54dd5510254a1ba0d58ce051cf8
sarkarmamun171 <sarkar.pau.cse171@gmail.com>
2026-08-03 22:51:44 +0600

    Login API Emd

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -28,7 +28,29 @@ class AuthController extends Controller
         ],201);
     }
 
-    public function login(Request $request){
-        $user = User::where('email', $request->email);
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+            if (!$user) {
+        return response()->json([
+            'message' => 'Invalid email or password'
+        ], 401);
+    }
+       if (!Hash::check($request->password, $user->password)) {
+        return response()->json([
+            'message' => 'Invalid email or password'
+        ], 401);
+    }
+       $token = $user->createToken('auth_token')->plainTextToken;
+            return response()->json([
+            'message' => 'Login successful',
+            'token' => $token,
+            'user' => $user,
+        ], 200);
     }
 }
